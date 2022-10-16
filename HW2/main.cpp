46bc6f22c7dd5ad04d0d5744088e71b5a014f842
ayevu105 <97065772+ayevu105@users.noreply.github.com>
2022-10-16 12:39:14 -0700

    Added skeleton code

---
+++ HW2/main.cpp
@@ -0,0 +1,42 @@
+
+
+#include "threadedBST.h"
+#include <iostream>
+
+using namespace std;
+
+void test1() { // size 1
+    int n = 1;
+    ThreadedBST origTree(n); // creating a tree
+    ThreadedBST newTree = origTree; // copying original tree
+    // remove all evens from newTree
+    for (int i = 2; i < n + 1; i += 2) {
+        newTree.remove(i);
+    }
+    //output
+    cout << "Original Tree: " << origTree << endl;
+    cout << "New Tree: " << newTree << endl << endl;
+    
+}
+
+void test2() { // size 4
+    int n = 4;
+    ThreadedBST origTree(n); // creating a tree
+    ThreadedBST newTree = origTree; // copying original tree
+    // remove all evens from newTree
+    for (int i = 2; i < n + 1; i += 2) {
+        newTree.remove(i);
+    }
+    //output
+    cout << "Original Tree: " << origTree << endl;
+    cout << "New Tree: " << newTree << endl << endl;
+}
+
+int main() {
+    
+    test1();  
+    test2();  
+
+    return 0;
+}
+
---
+++ HW2/threadedBST.cpp
@@ -0,0 +1,110 @@
+
+
+#include "threadedBST.h"
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// output overload
+ostream& operator<<(ostream& out, const ThreadedBST& bst) {
+
+}
+
+// assignment overload for copying
+ThreadedBST& ThreadedBST::operator=(const ThreadedBST& bst) {
+
+}
+
+// constructor
+// n equals the total number of nodes in the tree
+ThreadedBST::ThreadedBST(int n) : totalNodes{ n } {
+	// vector for adding inputs
+	vector<int> nums;
+	for (int i = 1; i <= n; i++) {
+		nums.push_back(i);
+	}
+	// assign headptr and start recursive call to build tree, then thread it
+	headPtr = buildSubTree(nums, 0, nums.size() - 1);
+	threadTree(headPtr);
+}
+
+// helper function to thread a tree
+void ThreadedBST::threadTree(Node* headPtr) {
+	// find left most child of tree 
+	Node* currentNode = headPtr;
+	while (currentNode->leftPtr != nullptr) {
+		currentNode = currentNode->leftPtr;
+	}
+	if (currentNode->rightPtr != nullptr) {
+		currentNode = currentNode->rightPtr;
+	}
+	// traverse tree and create threads when necessary
+	while (currentNode->value < totalNodes) {
+		if (currentNode->rightPtr == nullptr) { // thread needed
+			// create thread and follow it
+			currentNode->rightPtr = getEntry(currentNode->value + 1);
+			currentNode->isThread = true;
+			currentNode = currentNode->rightPtr;
+
+			// new currentNode will always have a right child, follow again
+			currentNode = currentNode->rightPtr;
+		}
+		else if (currentNode->leftPtr != nullptr) {
+			// explore left child/subtree
+			currentNode = currentNode->leftPtr;
+		}
+		else {
+			// explore right child/subtree
+			currentNode = currentNode->rightPtr;
+		}
+	}
+}
+
+// helper function to build subtrees recursively
+// returns a Node* to make recursion possible
+Node* ThreadedBST::buildSubTree(const vector<int>& nums,
+	int lowerIndex, int upperIndex) {
+}
+
+// copy constructor
+ThreadedBST::ThreadedBST(const ThreadedBST& tbst) {
+
+}
+
+ 
+
+
+// destructor
+ThreadedBST::~ThreadedBST() {
+}
+
+// clear helper function to recursively delete TBST
+void ThreadedBST::clear(Node* node) {
+
+
+}
+
+// searches Tree for a node with specified value
+// returns Node if found, nullptr if not found
+Node* ThreadedBST::getEntry(int n) const {
+
+}
+
+
+// returns true if empty, false if not
+bool ThreadedBST::isEmpty() const {
+	return (headPtr == nullptr);
+}
+
+// iterator to do inorder traversal of the tree
+vector<int> ThreadedBST::inorderTraversal() const {
+
+}
+
+//removes a node
+Node* ThreadedBST::remove(int value) {
+
+}
+
